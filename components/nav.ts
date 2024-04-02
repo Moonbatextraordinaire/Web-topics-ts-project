8d0a487b53e26ffe6800f89f1c143c508124e4a8
tanguy <tanguy.clavareau@gmail.com>
2024-04-03 01:07:53 +0200

    nav

---
+++ components/nav.ts
@@ -0,0 +1,16 @@
+export const nav = ` 
+<nav class="header">
+    <div class="logo">
+        <a href="/">
+            <img src="/img/logo.png" alt="Website logo of a stylised letter T">
+        </a>
+    </div>
+    <div class="navigation">
+        <ul>
+            <li><a href="/create/">Create</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Get in touch</a></li>
+        </ul>
+    </div>
+</nav>
+`;

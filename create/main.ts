2a9a28c17ba8ad7803d36ae6e93354d7f7217079
tanguy <tanguy.clavareau@gmail.com>
2024-04-03 12:01:38 +0200

    a

---
+++ create/main.ts
@@ -0,0 +1,4 @@
+import "/css/style.css";
+import { nav } from "../components/nav";
+
+(document.querySelector("header") as HTMLDivElement).innerHTML = nav;
--- homepage/home.ts
+++ homepage/home.ts
@@ -1,52 +1,62 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import SplitType from 'split-type'
+import SplitType from "split-type";
+import "/css/style.css";
+import { nav } from "../components/nav";
 
-    const introText: SplitType = new SplitType('h1', { types: 'words' });
-    const words: any = introText.words;
-    gsap.registerPlugin(ScrollTrigger);
+(document.querySelector("header") as HTMLDivElement).innerHTML = nav;
 
-    let timeLine = gsap.timeline();
-    
-    timeLine.fromTo(
-        words,
-        { y: 100, opacity: 0 },
-        { y: 0, opacity: 1, stagger: 0.7, duration: 1, ease: 'power4.out' }
-    )
-    timeLine.fromTo(".arrow",
-    {y: -200, opacity: 0,display:"none"},
-        { y: 0, display:"inline",opacity: 1, duration: 2, ease: 'bounce.out' }
-        )
-    timeLine.to(words,
-    {
-        scrollTrigger: {
-            trigger: "h1",
-            start: "top center-=40%",  
-            end: "top center+=10%",
-            scrub: true,
-            onLeave: () => gsap.to("h1", { opacity: 0 }),
-            onEnterBack: () => gsap.to("h1", { opacity: 1 }),
-        }
-    }
+gsap.registerPlugin(ScrollTrigger);
+
+const palette = ["#e51531", "#2a70ae", "#fab511"];
+const introText: SplitType = new SplitType("h1", { types: "words" });
+const words: any = introText.words;
+
+let timeLine = gsap.timeline();
+
+words.forEach((word) => {
+  timeLine.fromTo(
+    word,
+    { y: 100, opacity: 0, color: palette[words.indexOf(word)] },
+    { y: 0, opacity: 1, stagger: 0.7, duration: 1, ease: "power4.out" }
+  );
+});
+
+timeLine.fromTo(
+  ".arrow",
+  { y: -200, opacity: 0, display: "none" },
+  { y: 0, display: "inline", opacity: 1, duration: 1, ease: "bounce.out" }
 );
-    timeLine.to(".arrow",
-        {
-            scrollTrigger: {
-                trigger: ".arrow",
-                start: "top+=10vh",
-                end: "bottom-=2.6vh",
-                scrub: true,
-                onEnter: () => gsap.to(".arrow", { opacity: 1 }),
-                onLeave: () => gsap.to(".arrow", { opacity: 0 }),
-            }
-        }
-    );
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        const hamburger = document.querySelector('.hamburger') as HTMLButtonElement;
-        const navMenu = document.getElementById('nav-menu') as HTMLElement;
 
-        hamburger.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-        });
-    });
+timeLine.to(words, { color: "#1d1d1b", duration: 2, ease: "power4.out" });
+timeLine.to(words, {
+  scrollTrigger: {
+    trigger: "h1",
+    start: "top center-=40%",
+    end: "top center+=10%",
+    scrub: true,
+    onLeave: () => gsap.to("h1", { opacity: 0 }),
+    onEnterBack: () => gsap.to("h1", { opacity: 1 }),
+  },
+});
+timeLine.to(".arrow", {
+  scrollTrigger: {
+    trigger: ".arrow",
+    start: "top+=10vh",
+    end: "bottom-=2.6vh",
+    scrub: true,
+    onEnter: () => gsap.to(".arrow", { opacity: 1 }),
+    onLeave: () => gsap.to(".arrow", { opacity: 0 }),
+  },
+});
+timeLine.to(".explore", {
+  scrollTrigger: {
+    trigger: "h2",
+    start: "top center",
+    end: "bottom center",
+    scrub: 2,
+    pin: true,
+  },
+  background: "#2a70ae",
+  color: "#f9f0de",
+});
